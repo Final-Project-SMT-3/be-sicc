fc8d06adb0478d3555a7db3f9d1e45d93b07f940
LuciferLovesMe <abhiemsmile@gmail.com>
2023-10-31 14:41:43 +0700

    API/Login: create register api

--- App/Models/UsersModel.php
+++ App/Models/UsersModel.php
@@ -69,4 +69,70 @@ class UsersModel{
         }
 
     }
+
+    public function register($request = []){
+        $param = new stdClass;
+        // Param for users / ketua
+        $nim = htmlspecialchars(trim($request['nim']));
+        $nama = htmlspecialchars(trim($request['nama']));
+        $username = htmlspecialchars(trim($request['username']));
+        $password = md5(htmlspecialchars(trim($request['password'])));
+
+        // Param for kelompok
+        $id_detail_lomba = htmlspecialchars(trim($request['id_lomba']));
+        $nama_kelompok = htmlspecialchars(trim($request['nama_kelompok']));
+        $nama_anggota = trim($request['nama_anggota']);
+        $nim_anggota = trim($request['nim_anggota']);
+        $id_mhs = null;
+
+        $this->conn->beginTransaction();
+        try{
+            $query = "INSERT INTO users(no_identitas, nama, username, password, tipe, created_at) value(:nim, :nama, :username, :password, 'mahasiswa', NOW())";
+            $result = $this->conn->prepare($query);
+            $result->bindParam(":nim", $nim);
+            $result->bindParam(":nama", $nama);
+            $result->bindParam(":username", $username);
+            $result->bindParam(":password", $password);
+            $res = $result->execute();
+            if($res){
+                $id_mhs = $this->conn->lastInsertId();
+                $queryKelompok = "INSERT INTO kelompok(id_mhs, id_detail_lomba, nama_kelompok, nim_anggota, nama_anggota, created_at) VALUES(:id_mhs, :id_detail_lomba, :nama_kelompok, :nim_anggota, :nama_anggota, NOW())";
+                $resultKelompok = $this->conn->prepare($queryKelompok);
+                $resultKelompok->bindParam(":id_mhs", $id_mhs);
+                $resultKelompok->bindParam(":id_detail_lomba", $id_detail_lomba);
+                $resultKelompok->bindParam(":nama_kelompok", $nama_kelompok);
+                $resultKelompok->bindParam(":nama_anggota", $nama_anggota);
+                $resultKelompok->bindParam(":nim_anggota", $nim_anggota);
+                $resKelompok = $resultKelompok->execute();
+                $this->conn->commit();
+                if($resKelompok){
+                    $param->status = 200;
+                    $param->message = 'Berhasil menambahkan kelompok baru.';
+                    $param->response = '';
+                } else{
+                    $this->conn->rollBack();
+                    $param->status = 500;
+                    $param->message = 'Terjadi kesalahan.';
+                    $param->response = '';
+                }
+            } else{
+                $this->conn->rollBack();
+                $param->status = 500;
+                $param->message = 'Terjadi kesalahan.';
+                $param->response = '';
+            }
+        } catch(Exception $e){
+            $this->conn->rollBack();
+            $param->status = 500;
+            $param->message = 'Terjadi kesalahan. ' . $e->getMessage();
+            $param->response = null;
+        } catch(PDOException $e){
+            $this->conn->rollBack();
+            $param->status = 500;
+            $param->message = 'Terjadi kesalahan. ' . $e->getMessage();
+            $param->response = null;
+        } finally {
+            return json_encode($param);
+        }
+    }
 }
