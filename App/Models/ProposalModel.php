6600b586f39c0b743566309fed97b4342e81a670
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-11-28 23:33:31 +0700

    Last Change To API Upload Proposal

--- App/Models/ProposalModel.php
+++ App/Models/ProposalModel.php
@@ -7,7 +7,6 @@ class ProposalModel {
 
     public function __construct() {
         try{
-            $this->uploadPath = '../storage/proposal/';
             $this->conn = new PDO('mysql:host=localhost;dbname=db_cc', 'root', '');
         } catch(PDOException $e){
             die($e->getMessage());
@@ -18,62 +17,52 @@ class ProposalModel {
         $param = new stdClass;
         $id_dospem = htmlspecialchars(trim($request['id_dospem']));
         $id_judul = htmlspecialchars(trim($request['id_judul']));
-
-        // Getting file info from the request
-        $fileinfo = pathinfo($_FILES['pdf']['name']);
-        $extension = $fileinfo['extension'];
-        $file_path = $this->generateFileName() . '.' . $extension; // Unique file name
-
+        $file_path = htmlspecialchars(trim($request['file_path'])); // Getting file path from the request
+    
+        // You can perform additional validations on $file_path if needed
+    
         $this->conn->beginTransaction();
         try {
-            // Upload file to the storage path
-            move_uploaded_file($_FILES['pdf']['tmp_name'], $this->uploadPath . $file_path);
-
-            $query = "INSERT INTO submit_proposal(id_dospem, id_judul, path, created_at) VALUES (:id_dospem, :id_judul, :path_file, now())";
-            $result = $this->conn->prepare($query);
-            $result->bindParam(':id_dospem', $id_dospem);
-            $result->bindParam(':id_judul', $id_judul);
-            $result->bindParam('path_file', $file_path);
-            $res = $result->execute();
-            $this->conn->commit();
-
-            if($res){
-                $param->status_code = 200;
-                $param->message = 'Success';
-                $param->response = 'Berhasil Mengajukan Proposal Ke Dosen Pembimbing.';
+            $new_file_path = time() . '.pdf'; // Unique file name
+    
+            if (file_put_contents('../storage/proposal/'.$new_file_path, base64_decode($file_path))) {
+                $query = "INSERT INTO submit_proposal(id_dospem, id_judul, path, created_at) VALUES (:id_dospem, :id_judul, :path_file, now())";
+                $result = $this->conn->prepare($query);
+                $result->bindParam(':id_dospem', $id_dospem);
+                $result->bindParam(':id_judul', $id_judul);
+                $result->bindParam(':path_file', $new_file_path); // Corrected binding
+    
+                $res = $result->execute();
+                $this->conn->commit();
+    
+                if ($res) {
+                    $param->status_code = 200;
+                    $param->message = 'Success';
+                    $param->response = 'Berhasil Mengajukan Proposal Ke Dosen Pembimbing.';
+                } else {
+                    $this->conn->rollBack();
+                    $param->status_code = 500;
+                    $param->message = 'Terjadi kesalahan.';
+                    $param->response = '';
+                }
             } else {
                 $this->conn->rollBack();
                 $param->status_code = 500;
-                $param->message = 'Terjadi kesalahan.';
+                $param->message = 'Terjadi kesalahan saat menyalin file.';
                 $param->response = '';
             }
-        } catch(Exception $e){
+        } catch (Exception $e) {
             $this->conn->rollBack();
             $param->status_code = 500;
             $param->message = 'Terjadi kesalahan. ' . $e->getMessage();
             $param->response = '';
-        } catch(PDOException $e){
+        } catch (PDOException $e) {
             $this->conn->rollBack();
             $param->status_code = 500;
             $param->message = 'Terjadi kesalahan. ' . $e->getMessage();
             $param->response = '';
-        } finally{
+        } finally {
             return $param;
-        }
-    }
-
-    private function generateFileName() {
-        $query = "SELECT max(id) as id FROM submit_proposal";
-    
-        $result = $this->conn->prepare($query);
-        $result->execute();
-        $result->setFetchMode(PDO::FETCH_ASSOC);
-        $res = $result->fetchAll();
-    
-        if (empty($res) || $res[0]['id'] === null) {
-            return 1;
-        } else {
-            return intval($res[0]['id']) + 1;
         }
     }
 }
